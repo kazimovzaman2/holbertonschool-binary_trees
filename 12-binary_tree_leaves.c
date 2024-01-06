50e868cb2af48b6af24eb051fbbc060d82c9d2d0
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-01-06 19:37:34 +0400

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,13 +9,13 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t left = 0, right = 0;
 
 	if (!tree)
 		return (0);
 
 	if (!tree->left && !tree->right)
-		return (1 + left + right);
+		return (left + right + 1);
 
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
